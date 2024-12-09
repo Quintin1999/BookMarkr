957dc7a373ddf029f77a1a9eb328e4df877d3269
Alexandra Pierson <alexandrajpierson@gmail.com>
2024-12-08 18:30:40 -0600

    styling changes

--- src/perslib/PersonalLibrary.tsx
+++ src/perslib/PersonalLibrary.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import BookGrid from "../components/bookGrid/BookGrid";
+import styles from "./personalLibrary.module.css";
 
 interface Book {
   _id: string;
@@ -21,19 +22,22 @@ const PersonalLibraryPage: React.FC = () => {
       }
 
       try {
-        const response = await fetch("http://localhost:3000/api/users/library", {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const response = await fetch(
+          "http://localhost:3000/api/users/library",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
         if (!response.ok) {
           throw new Error("Failed to fetch personal library");
         }
 
         const data: Book[] = await response.json();
-        console.log("Fetched Books:",data)//debugging
+        console.log("Fetched Books:", data); //debugging
         setBooks(data);
       } catch (error) {
         console.error("Error fetching personal library:", error);
@@ -48,14 +52,16 @@ const PersonalLibraryPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>My Personal Library</h1>
-      {books.length > 0 ? (
-        <BookGrid books={books} onAdd={handleAddBook} />
-      ) : (
-        <p>You should add books!</p>
-      )}
-    </div>
+    <main className="container">
+      <div className={`${styles.libraryGrid}`}>
+        <h1>My Personal Library</h1>
+        {books.length > 0 ? (
+          <BookGrid books={books} onAdd={handleAddBook} />
+        ) : (
+          <p>You should add books!</p>
+        )}
+      </div>
+    </main>
   );
 };
 
